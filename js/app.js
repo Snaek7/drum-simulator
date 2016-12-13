14b7056ae206067a9babe564ccd94c8ff9ec5536
Snaek7 <emerson_franca77@hotmail.com>
2016-12-13 10:44:15 -0800

    set current time to 0

--- js/app.js
+++ js/app.js
@@ -25,46 +25,55 @@ keys.forEach(function(key){
 		console.log(key);
 		if(key.id =="clap"){
 			const audioClap = document.querySelector(`audio[data-key="65"]`);
+			audioClap.currentTime = 0;
 			audioClap.play();
 		}
 
 		if (key.id =="tinn") {
 			const audioTinn = document.querySelector(`audio[data-key="83"]`);
+			audioTinn.currentTime = 0;
 			audioTinn.play();
 		}
 
 		if (key.id =="boom") {
 			const audioBoom = document.querySelector(`audio[data-key="68"]`);
+			audioBoom.currentTime = 0;
 			audioBoom.play();
 		}
 
 		if (key.id =="tumm") {
 			const audioTumm = document.querySelector(`audio[data-key="70"]`);
+			audioTumm.currentTime = 0;
 			audioTumm.play();	
 		}
 
 		if (key.id =="tomm") {
 			const audioTomm = document.querySelector(`audio[data-key="71"]`);
+			audioTomm.currentTime = 0;
 			audioTomm.play();
 		}
 
 		if (key.id =="tumm2") {
 			const audioTumm2 = document.querySelector(`audio[data-key="72"]`);
+			audioTumm2.currentTime = 0;
 			audioTumm2.play();
 		}
 
 		if (key.id=="dee") {
 			const audioDee = document.querySelector(`audio[data-key="74"]`);
+			audioDee.currentTime = 0;
 			audioDee.play();
 		}
 
 		if (key.id=="tom") {
 			const audioTom = document.querySelector(`audio[data-key="75"]`);
+			audioTom.currentTime = 0;
 			audioTom.play();
 		}
 
 		if (key.id="tum") {
 			const audioTum = document.querySelector(`audio[data-key="76"]`);
+			audioTum.currentTime = 0;
 			audioTum.play();
 		}
 	});
